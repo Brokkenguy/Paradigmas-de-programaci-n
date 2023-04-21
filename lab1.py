c5d594752f83e651d56bb5447e221a1394b5d2e9
Brokkenguy <fisicsq@gmail.com>
2023-04-21 12:32:26 -0600

    seguimiento del laboratorio 3

--- lab1.py
+++ lab1.py
@@ -419,4 +419,155 @@ numNames = { 1:'One', 2: 'Two', 3: 'Three'}
 for pair in numNames.items():
     print(pair)
 
+#========================
+# Bucle sobre diccionario
+# Key = llave 
+# value = valor
+#========================
 
+for k,v in numNames.items():
+    print("key = ", k, ", value =", v)
+
+#=====================
+#  Primera función
+#=====================
+def saludo():
+    #=====================================
+    #  Documentación rápida de la función
+    #=====================================
+    """Esta función saluda"""
+    print('¡Qiúboles!, ¿cómo estás')
+
+#=========================
+#  Llamado de la función
+#=========================
+saludo()
+
+#===============================
+# se ejecuta pero no se asigna 
+#===============================
+salida = saludo()
+
+#====================
+#  Esto no funciona
+#====================
+print(salida)
+
+#==========================
+#  Mostrar documentación
+#==========================
+#help(saludo)
+
+#===========================
+#  Fucnión con argumento
+#===========================
+def salu2(nombre):
+    """Esta función te saluda por tu nombre"""
+    print("¡Qué onda ese",nombre,"!")
+salu2("Julián")
+salu2("Ángel")
+
+#==========================================
+#  Ahorrar trabajo del intérprete
+#  nombre:str  la variable nombre es un str
+#==========================================
+def saludos(nombre:str):
+    """Esta función te saluda por tu mobre estrictamente"""
+    print("¡Que onda ese",nombre,"!")
+saludos("Julián")
+a = 123
+print(type(a))
+saludos(a)
+
+#=====================================
+#  Función con muchos argumentos
+#=====================================
+def saludos_multiples(nombre1,nombre2,nombre3):
+    """Esta función saluda a 3 personas al mismo tiempo"""
+    print("Hola ",nombre1, ",",nombre2,"y",nombre3)
+saludos_multiples("Hugo","Paco","Luis")
+
+#=============================================
+#  Función con cualquier numero de argumentos
+#=============================================
+def muchos_saludos(*nombres):
+    """ESta función saluda a todos los que quieras"""
+    i = 0
+    #====================================
+    # end= es para ponerlo todo corrido
+    #====================================
+    print("Hola ", end="")
+    while len(nombres) > i:
+        # Último nombre 
+        if (i==len(nombres)-1):
+            print(nombres[i])
+        else:
+            # Cualquier otro nombre
+            print(nombres[i], end=", ")
+        i+=1
+
+muchos_saludos("Bosco","Angel","David","Tamara","Mili","Edwin","Lev","Luis","Abigail")
+
+def greet(firstname, lastname):
+    print('Hello', firstname , lastname)
+
+#================================================
+#  Llamar la función con argumentos en desorden
+#================================================
+greet(lastname='Jobs', firstname='Steve') # Se pueden especificar las variables en desorden
+
+#======================================
+# Función con argumentos escondidos **
+#======================================
+def greet(**person):
+    #=====================================================
+    # persona tiene características firstname y lastname
+    #=====================================================
+    print('Hello', person['firstname'], person['lastname'])
+
+greet(fisrtname='Steve', lastname='Jobs')
+greet(lastname='Jobs', firstname='Steve')
+greet(firstname='Bill', lastname='Gates', age=55) # Se pueden pasar más parámetros de los necesarios 
+
+#==================================
+#  Función con valores por defecto
+#==================================
+def greet(name = 'Guest'):
+    print('Hello', name)
+
+greet()  # Utiliza el valor dado de antemano
+greet('Steve')
+
+#=========================
+# Función con resultado
+#=========================
+def suma(a ,b):
+    return a + b
+
+#====================================
+#  Programación funcional
+#  Se pueden funciones en funciones
+#====================================
+total=suma(5, suma(10,20))
+print(total)
+
+#===================================================
+#  Cálculo lambda
+#  nombre de la función = lambda variable : función
+#===================================================
+x_al_cuadrado = lambda x : x * x
+a1 = x_añ_cuadrado(5)
+print(a1)
+
+#=============================
+# Lambda de varias variables
+#=============================
+suma = lambda x1, x2, x3: x1+x2+x3
+print(suma(99,98,97))
+
+sumas = lambda *x: x[0]+x[1]+x[2]+x[3]
+
+print (sumas(100,200,300,400))
+
+
+#=================================
