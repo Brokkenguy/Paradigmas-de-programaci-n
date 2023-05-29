a0d3e3ba59b61a48895d8732a9431490c4fcf3ea
Brokkenguy <fisicsq@gmail.com>
2023-05-29 12:44:32 -0600

    Areglando errores de la curva

--- OBJETOS/curva/Curva.py
+++ OBJETOS/curva/Curva.py
@@ -30,66 +30,67 @@ class Curva:
         s.lista_de_puntos()
         s.longitud()
 
-   #==================
-   # Lista de puntos
-   #==================
-   def lista_de_puntos(s) -> str:
+    #==================
+    # Lista de puntos
+    #==================
+    def lista_de_puntos(s) -> str:
+    
        
-       print(f"Número de puntos = {str(s.n)}")
+        print(f"Número de puntos = {str(s.n)}")
+    
+        # Formato de datos
+        s.formato = ""
+        for j in range(s.dim):
+            s.formato += "%15.8e"
+        s.formato += "\n"
+    
+        # Tupla de variables a imprimir
+        for i in range(0,s.n):
+            s.tup = (s.x[i],)
+            for ii in range(1,s.dim):
+                s.tup = s.tup + (s.x[i+ii*s.n],)
+            print(s.formato % s.tup)
+    
+    #=========================
+    # Longitud punto a punto
+    #=========================
+    def longitud(s) -> None:
+        t:np.float64 = 0.0
+        for i in range(0,s.n):
+            ip1 = i+1
+            if i == s.n-1:
+                ip1 = 0
+            d:np.float64 = (s.x[ip1]-s.x[i])**2
+            for j in range(1,s.dim):
+                d += (s.x[ip1+j*s.n]-sx[i+j*s.n])**2
+            t += d**0.5
+            s.l.append(t)
+        s.L = t
+        s.dx = t/float(s.n)
 
-       # Formato de datos
-       s.formato = ""
-       for j in range(s.dim):
-           s.formato += "%15.8e"
-       s.formato += "\n"
+    #================
+    # interpolación
+    #================
+    def interpolacion(s,p:int=0,r:float=0.0) -> list:
+        """ r es el parámetro spbre la curva [0,1)
+            p es la suavidad de la curva """
 
-       # Tupla de variables a imprimir
-       for i in range(0,s.n):
-           s.tup = (s.x[i],)
-           for ii in range(1,s.dim):
-               s.tup = s.tup + (s.x[i+ii*s.n],)
-           print(s.formato % s.tup)
-
-   #=========================
-   # Longitud punto a punto
-   #=========================
-   def longitud(s) -> None:
-       t:np.float64 = 0.0
-       for i in range(0,s.n):
-           ip1 = i+1
+        rdx:np.float64 = 1.0/s.dx
+        xi:float = []
+        i:np.int32 = int(r*s.L*rdx)
+        a:np.float64 = r*s.L*rdx - float(i)  # Distancia normalizada
+ 
+        #=========================
+        # Interpolación lineal C0
+        #=========================
+        if p == 0:
+           ip1:np.int32 = i+1
            if i == s.n-1:
-               ip1 = 0
-           d:np.float64 = (s.x[ip1]-s.x[i])**2
+              ip1 = 0
+           xi.append(a*s.x[ip1] + (1.0-a)*s.x[i])
            for j in range(1,s.dim):
-               d += (s.x[ip1+j*s.n]-sx[i+j*s.n])**2
-           t += d**0.5
-           s.l.append(t)
-       s.L = t
-       s.dx = t/float(s.n)
-
-   #================
-   # interpolación
-   #================
-   def interpolacion(s,p:int=0,r:float=0.0) -> list:
-       """ r es el parámetro spbre la curva [0,1)
-           p es la suavidad de la curva """
-
-       rdx:np.float64 = 1.0/s.dx
-       xi:float = []
-       i:np.int32 = int(r*s.L*rdx)
-       a:np.float64 = r*s.L*rdx - float(i)  # Distancia normalizada
-
-       #=========================
-       # Interpolación lineal C0
-       #=========================
-       if p == 0:
-          ip1:np.int32 = i+1
-          if i == s.n-1:
-             ip1 = 0
-          xi.append(a*s.x[ip1] + (1.0-a)*s.x[i])
-          for j in range(1,s.dim):
-              xi.append(a*s.x[ip1+j*s.n]+(1.0-a)*s.x[i+j*s.n])
-
+               xi.append(a*s.x[ip1+j*s.n]+(1.0-a)*s.x[i+j*s.n])
+ 
         #=========================
         # Interpolación cúbica C1
         #=========================
