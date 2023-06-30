7d7f027fa8941eb0c86298f2489d0f81bddad5c5
Brokkenguy <fisicsq@gmail.com>
2023-06-29 21:11:30 -0600

    Final del laboratorio 11

--- PARALELO/NUMBA/GPU/calcularpi.py
+++ PARALELO/NUMBA/GPU/calcularpi.py
@@ -17,3 +17,29 @@ def calcularpi_kernel(rng_states, iteraciones, out):
     # la fracción de ellos que cae dentro del círculo unitario
     cae_adentro = 0
     for i in range(iteraciones):
+        x = xoroshiro128p_uniform_float64(rng_states, ii)
+        y = xoroshiro128p_uniform_float64(rng_states, ii)
+        if x**2 + y**2 <= 1.0:
+            cae_adentro += 1
+    out[ii] = 4.0 * cae_adentro / iteraciones
+
+#----------------------
+#  Procesos para cuda
+#----------------------
+hilosporbloque = 64
+bloques = 24
+
+#-----------------------
+#  Arreglos necesarios
+#_----------------------
+seed1 = random.uniform(-1,1)
+seed2 = random.seed(seed1)
+seed = random.randit(0,1000)
+rng_states = create_xoroshiro128p_states(hilosporbloque*bloques, seed)
+out = np.zeros(hilosporbloque*bloques, dtype=np.float64)
+
+#-----------------------
+#  Llamar a la funcion
+#-----------------------
+calcularpi_kernel[bloques, hilosporbloque](rng_states, 10000, out)
+print("pi:", out.mean())
